58daf91b7b9117ea046acf3f877d4112efd2f504
vikash chauhan <vikashchauhan51@gmail.com>
2018-04-29 14:02:15 +0530

    DataCache first commit.

---
+++ DataCache/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCache
+{
+    class MyClass
+    {
+
+        public async Task<string> GetMessage()
+        {
+
+            return await Task.FromResult("Hello");
+        }
+        public async Task<string> GetMessage(string message)
+        {
+
+            return await Task.FromResult(message);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new MyClass();
+            var data = InMemoryCache.Memoize<string, string>(obj.GetMessage, DateTime.Now.AddMinutes(1))("demo");
+            Console.WriteLine(data.Result);
+            InMemoryCache.Clear<string, string>(obj.GetMessage)("demo").Wait();
+            var data1 = InMemoryCache.Memoize(obj.GetMessage, DateTime.Now.AddMinutes(1))();
+            Console.WriteLine(data1.Result);
+        }
+    }
+}
